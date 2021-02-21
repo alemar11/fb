0a92384d443f56a5d851f6a0c68278ed9b7be683
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-02-21 20:41:50 +0100

    DTS

---
+++ DTS/BackgroundURLSessionXCTests/BackgroundURLSessionXCTests/BackgroundURLSessionXCTests.h
@@ -0,0 +1,18 @@
+//
+//  BackgroundURLSessionXCTests.h
+//  BackgroundURLSessionXCTests
+//
+//  Created by Alessandro Marzoli on 30/11/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BackgroundURLSessionXCTests.
+FOUNDATION_EXPORT double BackgroundURLSessionXCTestsVersionNumber;
+
+//! Project version string for BackgroundURLSessionXCTests.
+FOUNDATION_EXPORT const unsigned char BackgroundURLSessionXCTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackgroundURLSessionXCTests/PublicHeader.h>
+
+
