8a4857a49ee1d6bb3bd05cd5a6a5df44e5398895
Alessandro Marzoli <alessandromarzoli@gmail.com>
2021-05-27 17:48:39 +0200

    refactor

--- DTS/BackgroundURLSessionXCTests/README.md
+++
@@ -1,2 +0,0 @@
-# BackgroundURLSessionXCTests
-XCTests with a  Background URLSession 
--- DTS/Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model.swift
-//  CoreDataMigrationWithDerivedAttributes
-//
-//  Created by Alessandro Marzoli on 27/05/21.
-//
-
-import Foundation
