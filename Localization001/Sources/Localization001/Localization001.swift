caf67e40fbec6431c201398b5c79e0a2074a9370
Alessandro Marzoli <alessandromarzoli@gmail.com>
2024-09-23 11:56:38 +0200

    --wip-- [skip ci]

--- Localization001/Sources/Localization001/Localization001.swift
+++ Localization001/Sources/Localization001/Localization001.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 
 public struct TestView: View {
+  public init() {}
   public var body: some View {
     Text(String(localized: "Hello", bundle: .module))
       .frame(width: 100, height: 100)
