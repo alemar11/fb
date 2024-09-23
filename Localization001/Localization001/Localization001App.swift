ac6f299777188d2dc866d1a52206c2808049b05d
Alessandro Marzoli <alessandromarzoli@gmail.com>
2024-09-23 10:28:01 +0200

    cleaning

---
+++ Localization001/Localization001/Localization001App.swift
@@ -0,0 +1,17 @@
+//
+//  Localization001App.swift
+//  Localization001
+//
+//  Created by Alessandro Marzoli on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Localization001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
